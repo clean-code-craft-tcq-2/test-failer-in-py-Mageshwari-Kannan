a395a2959d4aaf00b9038440d76e9cf2b604411e
Mageshwari Kannan <91052034+Mageshwari-Kannan@users.noreply.github.com>
2022-01-22 17:13:07 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -5,7 +5,7 @@ def print_color_map():
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
             strlen = len(f'{i * 5 + j} | {major} | {minor}')
-            assert (strlen = 20)
+            assert (strlen == 20)
             print(f'{i * 5 + j} | {major} | {minor}')
     return len(major_colors) * len(minor_colors)
 
